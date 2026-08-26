8baf2ef97c21e5d6893ce12c6e1ea4052224779c
analeao-dev <anaqueiroz.dev@gmail.com>
2026-08-25 22:21:38 -0300

    feat: adiciona conexão com banco de dados e health check para monitoramento

--- ProductsMicroService.Infrastructure/DependencyInjection.cs
+++ ProductsMicroService.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using ProductsMicroService.Infrastructure.DbContext;
 
 namespace ProductsMicroService.Infrastructure;
 
@@ -6,6 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddScoped<DapperDbContext>();
         return services;
     }
 }
